5362a236400f4e50c46bc922b8dbbfbff92ce27c
Amine <amineouzane10@gmail.com>
2024-12-06 22:09:30 +0100

    add Webconfig update

--- src/main/java/com/example/sketch20_backend/config/WebConfig.java
+++ src/main/java/com/example/sketch20_backend/config/WebConfig.java
@@ -12,7 +12,11 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("https://tissirelalia.com");
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowedOrigins("https://tissirelalia.com");
             }
         };
     }
