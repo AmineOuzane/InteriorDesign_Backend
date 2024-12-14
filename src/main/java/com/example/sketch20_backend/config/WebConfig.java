2dcfd27e3c1248549d87689ff2120ed50a8c5adb
Amine <amineouzane10@gmail.com>
2024-12-14 18:03:56 +0100

    working version i guess

--- src/main/java/com/example/sketch20_backend/config/WebConfig.java
+++ src/main/java/com/example/sketch20_backend/config/WebConfig.java
@@ -1,11 +1,8 @@
 package com.example.sketch20_backend.config;
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebConfig {
     @Bean
@@ -13,8 +10,14 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOrigins("*")
+//                        .allowedMethods("*")
+//                        .allowedHeaders("*")
+//                        .allowedOrigins("https://tissirelalia.com");
+//            }
                 registry.addMapping("/**")
-                        .allowedOrigins("*") // Replace with your frontend domain or allow everyone
+                        .allowedOrigins("https://tissirelalia.com") // Replace with your frontend domain
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
